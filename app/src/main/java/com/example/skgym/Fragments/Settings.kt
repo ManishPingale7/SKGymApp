2f6d666c7713c1502007be5c06e22e81f88c370a
Aditya Kadlag <adityakadlag2004@gmail.com>
2021-08-14 12:09:10 +0530

    settings layout done

--- app/src/main/java/com/example/skgym/Fragments/Settings.kt
+++ app/src/main/java/com/example/skgym/Fragments/Settings.kt
@@ -5,23 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProviders
 import com.example.skgym.R
+import com.example.skgym.databinding.FragmentSettingsBinding
+import com.example.skgym.di.component.DaggerFactoryComponent
+import com.example.skgym.di.modules.FactoryModule
+import com.example.skgym.di.modules.RepositoryModule
+import com.example.skgym.mvvm.repository.MainRepository
+import com.example.skgym.mvvm.viewmodles.MainViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class Settings : Fragment() {
+    private lateinit var viewModel: MainViewModel
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
+    lateinit var currentUser: FirebaseUser
+    var mAuth = FirebaseAuth.getInstance()
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+    ): View {
+
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        init()
+        binding.SignOutSettings.setOnClickListener {
+            viewModel.signOut()
+        }
+        return binding.root
     }
 
+    private fun init() {
+        val component: DaggerFactoryComponent = DaggerFactoryComponent.builder()
+            .repositoryModule(RepositoryModule(requireContext()))
+            .factoryModule(FactoryModule(MainRepository(requireContext())))
+            .build() as DaggerFactoryComponent
+        viewModel =
+            ViewModelProviders.of(this@Settings, component.getFactory()).get(MainViewModel::class.java)
+        mAuth = FirebaseAuth.getInstance()
+        currentUser = mAuth.currentUser!!
 
+
+    }
 }
