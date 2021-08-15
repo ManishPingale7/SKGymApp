3a033f06b132405f9f3ec344e75a3dcfeb777c66
Aditya Kadlag <adityakadlag2004@gmail.com>
2021-08-15 08:49:02 +0530

    branch 1/2 done

--- app/src/main/java/com/example/skgym/Fragments/Home.kt
+++ app/src/main/java/com/example/skgym/Fragments/Home.kt
@@ -1,14 +1,15 @@
 package com.example.skgym.Fragments
 
 
-import android.content.Intent
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProviders
-import com.example.skgym.activities.GetUserData
+import com.example.skgym.R
 import com.example.skgym.databinding.FragmentHomeBinding
 import com.example.skgym.di.component.DaggerFactoryComponent
 import com.example.skgym.di.modules.FactoryModule
@@ -17,6 +18,7 @@ import com.example.skgym.mvvm.repository.MainRepository
 import com.example.skgym.mvvm.viewmodles.MainViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+
 class Home : Fragment() {
     private lateinit var viewModel: MainViewModel
     private var _binding: FragmentHomeBinding? = null
@@ -24,17 +26,25 @@ class Home : Fragment() {
     lateinit var currentUser: FirebaseUser
     var mAuth = FirebaseAuth.getInstance()
 
-
-
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         init()
+        //Is branch Taken
+        val isBranchTaken: SharedPreferences =
+            requireActivity().getSharedPreferences("isBranchTaken", MODE_PRIVATE)
+        val editor2 = isBranchTaken.edit()
 
+        val isBTaken = isBranchTaken.getBoolean("isBranchTaken", false)
 
+        if (isBTaken) {
+            binding.becomeMember.text = resources.getString(R.string.become_a_member)
+        } else {
+            binding.becomeMember.text = resources.getString(R.string.selectBranch)
+
+        }
 
         return binding.root
     }
