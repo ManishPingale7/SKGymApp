93c8ace767b29cc6d834f42af1014758d83ba3b1
Aditya Kadlag <adityakadlag2004@gmail.com>
2021-08-15 09:41:30 +0530

    branch 1/2 done

--- app/src/main/java/com/example/skgym/Fragments/Home.kt
+++ app/src/main/java/com/example/skgym/Fragments/Home.kt
@@ -2,6 +2,7 @@ package com.example.skgym.Fragments
 
 
 import android.content.Context.MODE_PRIVATE
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -10,6 +11,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProviders
 import com.example.skgym.R
+import com.example.skgym.activities.GetBranch
 import com.example.skgym.databinding.FragmentHomeBinding
 import com.example.skgym.di.component.DaggerFactoryComponent
 import com.example.skgym.di.modules.FactoryModule
@@ -43,7 +45,16 @@ class Home : Fragment() {
             binding.becomeMember.text = resources.getString(R.string.become_a_member)
         } else {
             binding.becomeMember.text = resources.getString(R.string.selectBranch)
+        }
 
+        binding.becomeMember.setOnClickListener {
+            val text=binding.becomeMember.text.toString()
+            if (text == resources.getString(R.string.become_a_member)){
+
+            }else if (text==resources.getString(R.string.selectBranch)){
+                val intent= Intent(requireContext(),GetBranch::class.java)
+                startActivity(intent)
+            }
         }
 
         return binding.root
--- app/src/main/java/com/example/skgym/activities/GetBranch.kt
+++ app/src/main/java/com/example/skgym/activities/GetBranch.kt
@@ -1,12 +1,86 @@
 package com.example.skgym.activities
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProviders
 import com.example.skgym.R
+import com.example.skgym.databinding.ActivityGetBranchBinding
+import com.example.skgym.databinding.ActivityMainBinding
+import com.example.skgym.di.component.DaggerFactoryComponent
+import com.example.skgym.di.modules.FactoryModule
+import com.example.skgym.di.modules.RepositoryModule
+import com.example.skgym.mvvm.repository.MainRepository
+import com.example.skgym.mvvm.viewmodles.MainViewModel
+import com.google.firebase.auth.FirebaseAuth
 
 class GetBranch : AppCompatActivity() {
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var viewModel: MainViewModel
+    private lateinit var component: DaggerFactoryComponent
+    lateinit var binding: ActivityGetBranchBinding
+    var branchList = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_get_branch)
+        binding = ActivityGetBranchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+
+        val handler = Handler()
+        handler.postDelayed({ // Do something after 5s = 5000ms
+            binding.progressBarBranch.visibility = View.GONE
+        }, 3000)
+
+        viewModel.fetchBranchNames().observe(this, {
+            Log.d(TAG, "onCreateView: Size ${it.size}")
+            val arrayAdapter = ArrayAdapter(
+                this, R.layout.dropdownitem,
+                it.toArray()
+            )
+            branchList = it
+
+            arrayAdapter.notifyDataSetChanged()
+            binding.branchDataGet.setAdapter(arrayAdapter)
+        })
+
+
+        binding.continueBranch.setOnClickListener {
+            Log.d(TAG, "onCreate: ${branchList.size}")
+            val branch = binding.branchDataGet.text.toString()
+            if (branch.isNotEmpty()) {
+                if (branchList.contains(branch)) {
+                    Log.d(TAG, "onCreate: $branch")
+                } else {
+                    Toast.makeText(this, "Enter a Verified Branch", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Select Your Branch", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+    }
+
+    private fun init() {
+        val window: Window = this.window
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.my_statusbar_color)
+        mAuth = FirebaseAuth.getInstance()
+        component = DaggerFactoryComponent.builder()
+            .repositoryModule(RepositoryModule(this))
+            .factoryModule(FactoryModule(MainRepository(this)))
+            .build() as DaggerFactoryComponent
+        viewModel = ViewModelProviders.of(this, component.getFactory())
+            .get(MainViewModel::class.java)
     }
 }
