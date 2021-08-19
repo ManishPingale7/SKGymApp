1c7e966aa50f8a08d90bc574407900301c671cee
Aditya Kadlag <adityakadlag2004@gmail.com>
2021-08-19 16:33:47 +0530

    branch taking done

--- app/src/main/java/com/example/skgym/activities/MainActivity.kt
+++ app/src/main/java/com/example/skgym/activities/MainActivity.kt
@@ -54,12 +54,8 @@ class MainActivity : AppCompatActivity() {
     private fun init() {
 
         val window: Window = this.window
-
-
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-
         window.statusBarColor = ContextCompat.getColor(this, R.color.my_statusbar_color)
         mAuth = FirebaseAuth.getInstance()
         component = DaggerFactoryComponent.builder()
