9c0613c05225b9681de6eff8938add3d47a7aabf
Manish Pingale <manishpingale2005@gmail.com>
2021-10-05 16:19:59 +0530

    Added RazorPay

--- app/src/main/java/com/example/skgym/activities/CheckoutActivity.kt
+++ app/src/main/java/com/example/skgym/activities/CheckoutActivity.kt
@@ -1,6 +1,8 @@
 package com.example.skgym.activities
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import android.widget.Toast
@@ -15,12 +17,16 @@ import com.example.skgym.di.modules.FactoryModule
 import com.example.skgym.di.modules.RepositoryModule
 import com.example.skgym.mvvm.repository.MainRepository
 import com.example.skgym.mvvm.viewmodles.MainViewModel
+import com.razorpay.Checkout
+import com.razorpay.PaymentResultListener
+import org.json.JSONObject
 
 
-class CheckoutActivity : AppCompatActivity() {
+class CheckoutActivity : AppCompatActivity(), PaymentResultListener {
     private lateinit var viewModel: MainViewModel
     private lateinit var component: DaggerFactoryComponent
-    private val TAG = "mActivity"
+    private lateinit var checkout: Checkout
+    private var demo: Plan? = null
 
     lateinit var binding: ActivityCheckoutBinding
 
@@ -28,27 +34,15 @@ class CheckoutActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityCheckoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        demo = intent.getParcelableExtra("Plan")
         init()
 
-        Toast.makeText(
-            baseContext,
-            intent.getParcelableExtra<Plan>("Plan").toString(),
-            Toast.LENGTH_SHORT
-        )
-            .show()
 
-        var demo: Plan? =intent.getParcelableExtra<Plan>("Plan")
         setPlanLayout(demo)
-        binding.checkoutBtn.setOnStateChangeListener { active ->
-            Toast.makeText(
-                this,
-                "State: $active",
-                Toast.LENGTH_SHORT
-            ).show()
+        binding.checkoutBtn.setOnStateChangeListener {
+            setPayment()
+
         }
-
-
     }
 
     private fun init() {
@@ -65,9 +59,45 @@ class CheckoutActivity : AppCompatActivity() {
 
     }
 
-    fun setPlanLayout(demo: Plan?) {
-        binding.cardPlanNameD.text=demo?.name
-        binding.cardPlanDescText.text=demo?.desc
-        binding.isPersonalD.text=if(demo?.pt==true){"PT"}else{"Normal"}
+    private fun setPlanLayout(demo: Plan?) {
+        demo?.let {
+            binding.cardPlanNameD.text = demo.name
+            binding.cardPlanDescText.text = demo.desc
+            if (demo.pt == true) {
+                binding.isPersonalD.text = "PT"
+            } else {
+                binding.isPersonalD.text = "Normal"
+                binding.badgeGold.visibility = View.GONE
+
+            }
+            binding.cardFeesD.text = demo.fees
+            binding.subtotal.text = demo.fees
+            binding.totalFees.text = demo.fees
+        }
+    }
+
+    private fun setPayment() {
+        Log.d("TAG111", "setPayment: here")
+        checkout = Checkout()
+        checkout.setKeyID("rzp_test_MbMaA0scjOVfmP")
+
+        val jsonObject = JSONObject()
+        jsonObject.put("name", "BK Gym Membership")
+        jsonObject.put("theme.color", "#0093DD")
+        jsonObject.put("currency", "INR")
+        demo?.let {
+            jsonObject.put("amount", (demo!!.fees).toInt() * 100)
+        }
+
+        checkout.open(this, jsonObject)
+    }
+
+    override fun onPaymentSuccess(p0: String?) {
+        Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPaymentError(int: Int, p1: String?) {
+        Toast.makeText(this, "Failed: $p1", Toast.LENGTH_SHORT).show()
+
     }
 }
