c70e63c5dba05817c859084201b7a7cc81db822e
Aditya Kadlag <adityakadlag2004@gmail.com>
2021-10-22 16:32:07 +0530

    Current Plan view options added to Home,Home lay changed

---
+++ app/src/main/java/com/example/skgym/activities/PlansHistory.kt
@@ -0,0 +1,12 @@
+package com.example.skgym.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.skgym.R
+
+class PlansHistory : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plans_history)
+    }
+}
